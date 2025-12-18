5f3a0611afeb760fa2c883e4102c8a22b0472edc
bala-amypo <bala@amypo.in>
2025-12-18 08:52:24 +0000

    User commit

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -1,14 +1,17 @@
 package com.example.demo.model;
 
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 public class Employee{
     @Id
     private Long id;
     private String fullName;
+     @Column(unique=true)
+    private String email;
     private String ADMIN;
     private String STAFF;
     private String skills;
+    @Min(1)
     private Int maxWeeklyHours;
 
 }
